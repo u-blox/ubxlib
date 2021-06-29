ecbb826bc366c1936a35201754c930d077808451
Rob Meades <rob.meades@u-blox.com>
2021-06-29 18:30:46 +0100

    Try 8: "cmd /c start /W /B python ...", only flag "CREATE_NEW_PROCESS_GROUP".

--- port/platform/common/automation/u_process_wrapper.py
+++ port/platform/common/automation/u_process_wrapper.py
@@ -130,6 +130,7 @@ if __name__ == "__main__":
             CALL_LIST.append("/c")
             CALL_LIST.append("start")
             CALL_LIST.append("/B")
+            CALL_LIST.append("/W")
         if PROCESS_PYTHON:
             CALL_LIST.append(PROCESS_PYTHON)
         CALL_LIST.append(PROCESS_CHECKER)
@@ -165,7 +166,7 @@ if __name__ == "__main__":
                 # set this so that PROCESS_CHECKER pops up in another
                 # window and you can see what's going on after we've
                 # been killed
-                CREATION_FLAGS |= subprocess.DETACHED_PROCESS
+                # CREATION_FLAGS |= subprocess.DETACHED_PROCESS
                 CREATION_FLAGS |= subprocess.CREATE_NEW_PROCESS_GROUP
             # Set shell to True to keep Jenkins happy
             PROCESS = subprocess.Popen(u_utils.subprocess_osify(CALL_LIST, shell=True),
