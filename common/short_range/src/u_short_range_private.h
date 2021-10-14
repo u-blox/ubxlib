98a81cbe38dbe22217f4f63fdbc38c31cbbac9cf
Andreas Anderberg <andreas.anderberg@u-blox.com>
2021-10-14 14:51:53 +0200

    Replace license text for all files (#343)
    All headers should use license text "Copyright 202x u-blox"

--- common/short_range/src/u_short_range_private.h
+++ common/short_range/src/u_short_range_private.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 u-blox Ltd
+ * Copyright 2020 u-blox
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
