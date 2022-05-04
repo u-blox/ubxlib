12a30dba9a99b0184fab59a07f1c12cfa6ff3048
Andreas Anderberg <andreas.anderberg@u-blox.com>
2022-05-04 13:21:11 +0200

    automation: Cleanup of Docker related things + more Docker documentation (#537)
    Removed the Docker based agent since we don't use it. Keeping the host_setup.sh
    script though since it can be used for setting up Docker + docker-compose +
    udev rules for a development machine.
    
    Also added some more documention for Docker related things.

--- port/platform/common/automation/docker/host/host_setup.sh
+++ port/platform/common/automation/docker/host/host_setup.sh
@@ -1,10 +1,9 @@
 ###########################################################################
-# Script used for setting up a machine to run ubxlib agent
+# Script used for setting up a machine to run docker
 #
 # This script will:
 # * Install udev rules needed to access J-Link, ST-Link and FTDI devices
 # * Install docker + docker-compose
-# * Install SSH server
 ###########################################################################
 
 #!/bin/bash
@@ -25,19 +24,6 @@ read -p "Do you want to install Docker? (y/n) " RESP
 if [ "$RESP" = "y" ]; then
     ${BASEDIR}/scripts/install_docker.sh
     DOCKER_WAS_INSTALLED=1
-fi
-
-read -p "Do you want to install SSH server? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
-    sudo apt install openssh-server
-    # Add firewall rule for SSH
-    sudo ufw allow ssh
-fi
-
-read -p "Do want to allow Jenkins SSH login? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
-    # Add the Jenkins public key as autorized key
-    echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIApWpKvlmLFeYwL9dOFN8Jx2A+2ptkmqif2h347Oxs9B dummy@u-blox.com" >> $HOME/.ssh/authorized_keys
 fi
 
 # THIS MUST ALWAYS BE PLACED LASTLY IN THE SCRIPT!
