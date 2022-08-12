cc85b58e783e206af94f2417c85876178f321f68
Peter Lerup <plerup@users.noreply.github.com>
2022-08-12 13:13:09 +0200

    Improvements for using Ubxlib as a Zephyr module (#649)
    This change add some files that facilitates using Ubxlib as a Zephyr module.
    It is now possible to just add one include statement in the application CMake
    configuration in order to get full access to Ubxlib and with suitable KConfig
    settings. Example of a "helloworld" application:
    
    cmake_minimum_required(VERSION 3.13.1)
    
    include(../ubxlib_priv/zephyr/ubxlib.cmake)
    find_package(Zephyr HINTS $ENV{ZEPHYR_BASE})
    
    project(hello)
    target_sources(app PRIVATE src/main.c)

---
+++ zephyr/ubxlib.cmake
@@ -0,0 +1,15 @@
+
+# Append UBXLIB as a Zephyr module
+get_filename_component(UBXLIB_BASE "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+list(APPEND ZEPHYR_EXTRA_MODULES ${UBXLIB_BASE})
+if(NOT UBXLIB_NO_DEF_CONF)
+  # Add the UBXLIB default configuration variables
+  list(APPEND CONF_FILE "${UBXLIB_BASE}/port/platform/zephyr/default.conf")
+  # Ensure that possible application config can still override the defaults as
+  # West includes that file before the CONF_FILE list. It is ok to read the
+  # file twice and the last read will prevail
+  set(UBXLIB_APP_CONF "${CMAKE_SOURCE_DIR}/prj.conf")
+  if(EXISTS ${UBXLIB_APP_CONF})
+      list(APPEND CONF_FILE ${UBXLIB_APP_CONF})
+  endif()
+endif()
