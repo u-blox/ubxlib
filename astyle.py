c8c48bd411d93d76bf47fefd1d3c7603cb4b7463
Rob Meades <RobMeades@users.noreply.github.com>
2022-08-22 14:19:21 +0100

    Developer assist only: astyle.py (#658)
    Add a Python script, astyle.py, in the root of the ubxlib directory tree, which calls the AStyle command line to auto-correct all code in the ubxlib directory tree. Saves having to remember the command-line. AStyle 3.1 must be installed and on the path for this to work. Must be called from the ubxlib root directory; this way it won't go trying to AStyle-ify, recursively, all of the files from the directory you happen to be in at the time...

---
+++ astyle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+'''Run AStyle on all code.'''
+
+# Run this from the ubxlib root directory to run AStyle, which must be on the path,
+# on all relevant files in the ubxlib directory tree, using the standard ubxlib astyle.cfg
+
+import subprocess
+
+with subprocess.Popen("astyle --options=astyle.cfg --suffix=none --verbose " \
+                      "--errors-to-stdout --recursive *.c,*.h,*.cpp,*.hpp",
+                      stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                      universal_newlines=True) as astyle:
+    output = astyle.communicate()
+    for line in output:
+        print (line, end=" ")
