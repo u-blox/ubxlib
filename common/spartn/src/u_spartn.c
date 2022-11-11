1abd66417d1f13a2729a72a772bc408cebcff1cd
RobMeades <rob@meades.org>
2022-11-11 10:56:44 +0000

    Correct copyright headers.
    Some files had accidentally ended up with an ARM Apache 2.0 copyright header instead of a u-blox Apache 2.0 copyright header, now fixed.

--- common/spartn/src/u_spartn.c
+++ common/spartn/src/u_spartn.c
@@ -1,12 +1,11 @@
 /*
- * Copyright (c) 2017, Arm Limited and affiliates.
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2019-2022 u-blox
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
