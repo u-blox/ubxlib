u-blox/ubxlib

commit 7a10f763c61ca18fae3df97edb81fe650b51c002

Test change only: update deprecated OpenOCD commands for STM32F4. (#873)
Some of the OpenOCD commands used by the STM32F4 part of the test system (hal_serial and tpiu) are deprecated; the relevant Python script is now updated to use the correct commands.  Also the adapter type for the STLink V2 standalone debugger, which we might use in future, is added to the udev rules list.

port/platform/common/automation/tasks/stm32cubef4.py
@@ -88,7 +88,7 @@ def build(ctx, makefile_dir=DEFAULT_MAKEFILE_DIR, output_name=DEFAULT_OUTPUT_NAM
         # Seems it must be a relative path and `\` directory separators must NOT be used.
         build_dir = os.path.relpath(ctx.build_dir, makefile_dir).replace("\\", "/")
         build_cmd = f'make -j{jobs} UBXLIB_PATH={ctx.config.root_dir} OUTPUT_DIRECTORY={build_dir} '\
-                    f'CFLAGS=\'{cflags}\' {" ".join(ctx.stm32cubef4_env)}'
+                    f'CFLAGS=\"{cflags}\" {" ".join(ctx.stm32cubef4_env)}'
         if is_static_analyze:
             ctx.analyze_dir = f"{ctx.build_dir}/analyze"
             check_proc = ctx.run(f'CodeChecker check -b "{build_cmd}" -o {ctx.analyze_dir} ' \
@@ -128,7 +128,7 @@ def flash(ctx, file=DEFAULT_FLASH_FILE, debugger_serial="",
     build_dir = Path(build_dir, output_name).absolute().as_posix()
     cmds = OPENOCD_DISABLE_PORTS_CMDS
     if debugger_serial != "":
-        cmds.append(f'hla_serial {debugger_serial}')
+        cmds.append(f'adapter serial {debugger_serial}')
     cmds += [
         f'program {build_dir}/{file} reset',
         'exit'
@@ -157,17 +157,22 @@ def log(ctx, debugger_serial="", port=40404,
 
     cmds = OPENOCD_DISABLE_PORTS_CMDS
     if debugger_serial != "":
-        cmds.append(f'hla_serial {debugger_serial}')
+        cmds.append(f'adapter serial {debugger_serial}')
     cmds += [
         'init',
-        f'tpiu config internal :{port} uart off \$_TARGET_SYSTEM_FREQUENCY \$_TARGET_SWO_FREQUENCY',
+        f'stm32f4x.tpiu configure -output :{port}',
+        'stm32f4x.tpiu configure -protocol uart',
+        'stm32f4x.tpiu configure -formatter 0',
+        'stm32f4x.tpiu configure -traceclk \$_TARGET_SYSTEM_FREQUENCY',
+        'stm32f4x.tpiu configure -pin-freq \$_TARGET_SWO_FREQUENCY',
+        'stm32f4x.tpiu enable',
         'itm port 0 on',
         'reset init',
         'resume'
     ]
     args = _to_openocd_args(cmds)
     promise = ctx.run(f'openocd -f {u_utils.OPENOCD_CFG_DIR}/stm32f4.cfg {args}', asynchronous=True)
-    # Let OpenOCD startup first
+    # Let OpenOCD start up first
     time.sleep(5)
     try:
         line = ""

port/platform/common/automation/53_dut.rules
@@ -28,3 +28,8 @@ SUBSYSTEM=="tty", ATTRS{idVendor}=="1366", ATTRS{idProduct}=="1015", ENV{ID_SEGG
 # SEGGER JLink, as used on the Nordic NRF5340DK board
 SUBSYSTEMS=="usb", ENV{ID_SEGGER_JLINK_NRF5240DK_INTERFACE_NUMBER}="$attr{bInterfaceNumber}", MODE:="0666", SYMLINK+="segger_jlink_nrf5340"
 SUBSYSTEM=="tty", ATTRS{idVendor}=="1366", ATTRS{idProduct}=="1055", ENV{ID_SEGGER_JLINK_NRF5240DK_INTERFACE_NUMBER}=="06", MODE:="0666", SYMLINK+="segger_jlink_nrf5340_uart"
+
+# STLink V2.1 standalone debugger
+SUBSYSTEMS=="usb", ENV{ID_STLINK_V2_1_STANDALONE_INTERFACE_NUMBER}="$attr{bInterfaceNumber}", MODE:="0666", SYMLINK+="stlink_v2_1_standalone"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", ENV{ID_STLINK_V2_1_STANDALONE_INTERFACE_NUMBER}=="07", MODE:="0666", SYMLINK+="stlink_v2_1_standalone_uart"
+

commit 25092755fc219529c425f4c5dca22543537563bc

Test fix only: instance 6.2 static analysis. (#874)
Commit 7a10f763c61ca18fae3df97edb81fe650b51c002 broke the STM32F4-based static analyzer (test instance 6.2), hereby fixed.

port/platform/common/automation/tasks/stm32cubef4.py
@@ -87,9 +87,13 @@ def build(ctx, makefile_dir=DEFAULT_MAKEFILE_DIR, output_name=DEFAULT_OUTPUT_NAM
         # OUTPUT_DIRECTORY is very picky in Windows.
         # Seems it must be a relative path and `\` directory separators must NOT be used.
         build_dir = os.path.relpath(ctx.build_dir, makefile_dir).replace("\\", "/")
-        build_cmd = f'make -j{jobs} UBXLIB_PATH={ctx.config.root_dir} OUTPUT_DIRECTORY={build_dir} '\
-                    f'CFLAGS=\"{cflags}\" {" ".join(ctx.stm32cubef4_env)}'
         if is_static_analyze:
+            # We do build_cmd differently for static analysis (single quotes
+            # around the cflags rather than double); the double quotes are
+            # the only ones that work with make on Windows, the single ones
+            # are the only ones that work with the static analyzer on Linux.
+            build_cmd = f'make -j{jobs} UBXLIB_PATH={ctx.config.root_dir} OUTPUT_DIRECTORY={build_dir} '\
+                        f'CFLAGS=\'{cflags}\' {" ".join(ctx.stm32cubef4_env)}'
             ctx.analyze_dir = f"{ctx.build_dir}/analyze"
             check_proc = ctx.run(f'CodeChecker check -b "{build_cmd}" -o {ctx.analyze_dir} ' \
                                  f'--config {u_utils.CODECHECKER_CFG_FILE} -i {u_utils.CODECHECKER_IGNORE_FILE}', warn=True)
@@ -98,6 +102,8 @@ def build(ctx, makefile_dir=DEFAULT_MAKEFILE_DIR, output_name=DEFAULT_OUTPUT_NAM
             elif check_proc.exited >= 128:
                 raise Exit("CodeChecker fatal error")
         else:
+            build_cmd = f'make -j{jobs} UBXLIB_PATH={ctx.config.root_dir} OUTPUT_DIRECTORY={build_dir} '\
+                        f'CFLAGS=\"{cflags}\" {" ".join(ctx.stm32cubef4_env)}'
             ctx.run(build_cmd)
 
 @task(