ff1e7796e2ffd75d3c039aebe13fac9447d28d19
Rob Meades <RobMeades@users.noreply.github.com>
2023-07-28 08:10:03 +0100

    BREAKING CHANGE to uPortGpio API: add index for Linux. (#947)
    The GPIO configuration structure gains an "index" element, which is used on Linux to allow the application to indicate the index of the GPIO chip a given pin is attached to e.g. for gpiochip0 on a BCM-based Raspberry Pi the index would be 0 (assuming U_PORT_GPIO_CHIP_NAME_BASE is "gpiochip", fixed at compile time).
    
    The U_PORT_GPIO_CONFIG_DEFAULT and U_PORT_GPIO_SET_DEFAULT macros are updated to set a default index of -1, which will be interpreted as "use the default chip" (which is gpiochip0).
    
    Provided you have always initialised any GPIO configuration structure to U_PORT_GPIO_CONFIG_DEFAULT, or if you are not using Linux, this constitutes no material change.
    
    Note: this means that if you are setting an output that must begin high, and hence you call uPortGpioSet() _before_ uPortGpioConfig(), that pin must be on the default GPIO chip.

--- port/platform/linux/src/u_port_gpio.c
+++ port/platform/linux/src/u_port_gpio.c
@@ -15,7 +15,9 @@
  */
 
 /** @file
- * @brief Implementation of the port GPIO API on Linux.
+ * @brief Implementation of the port GPIO API on Linux.  This implementation
+ * uses the gpiod library from the Linux kernel, hence libgpiod-dev must
+ * be installed.
  */
 
 #ifdef U_CFG_OVERRIDE
@@ -33,17 +35,40 @@
 #include "u_port_private.h"
 #include "u_compiler.h"
 
-/* This implementation uses the gpiod library from the Linux kernel,
-   hence libgpiod-dev must be installed.
-*/
-
-
 /* ----------------------------------------------------------------
  * COMPILE-TIME MACROS
  * -------------------------------------------------------------- */
 
-#ifndef U_PORT_GPIO_CHIP_NAME
-# define U_PORT_GPIO_CHIP_NAME "gpiochip0"
+#ifndef U_PORT_GPIO_CHIP_NAME_BASE
+/** The base chip name, expected to be followed by the index to
+ * form, for example, gpiochip0, the maximum size of which (with
+ * index attached) must be less than #U_PORT_GPIO_CHIP_NAME_MAX_LENGTH.
+ */
+# define U_PORT_GPIO_CHIP_NAME_BASE "gpiochip"
+#endif
+
+#ifndef U_PORT_GPIO_CHIP_INDEX_DEFAULT
+/** The default chip index: gpiochip0
+ */
+# define U_PORT_GPIO_CHIP_INDEX_DEFAULT 0
+#endif
+
+#ifndef U_PORT_GPIO_CHIP_NAME_MAX_LENGTH
+/** The maximum length of a GPIO chip name, for example gpiochip0.
+ */
+# define U_PORT_GPIO_CHIP_NAME_MAX_LENGTH 16
+#endif
+
+#ifndef U_PORT_GPIO_CHIP_MAX_NUM
+/** The maximum number of GPIO chips.
+ */
+# define U_PORT_GPIO_CHIP_MAX_NUM 8
+#endif
+
+#ifndef U_PORT_GPIO_PIN_MAX_NUM
+/** The maximum number of GPIO pins.
+ */
+# define U_PORT_GPIO_PIN_MAX_NUM 128
 #endif
 
 #ifndef U_PORT_GPIO_CONSUMER_NAME
@@ -64,20 +89,33 @@
  * VARIABLES
  * -------------------------------------------------------------- */
 
-static struct gpiod_chip *gpGpioChip = NULL;
+/** Array of GPIO chip index for each GPIO pin.
+ */
+static int32_t gPinIndex[U_PORT_GPIO_PIN_MAX_NUM] = {0};
+
+/** Array of open GPIO chips.
+ */
+static struct gpiod_chip *gpGpioChip[U_PORT_GPIO_CHIP_MAX_NUM] = {0};
 
 /* ----------------------------------------------------------------
  * STATIC FUNCTIONS
  * -------------------------------------------------------------- */
 
-static struct gpiod_line *getChip_line(int32_t pin)
+/* Return the line for a pin, opening the GPIO chip if necessary. */
+static struct gpiod_line *pGetChipLine(int32_t pin)
 {
     struct gpiod_line *pLine = NULL;
-    if (gpGpioChip == NULL) {
-        gpGpioChip = gpiod_chip_open_by_name(U_PORT_GPIO_CHIP_NAME);
-    }
-    if (gpGpioChip != NULL) {
-        pLine = gpiod_chip_get_line(gpGpioChip, (unsigned int)pin);
+    char gpioChipName[U_PORT_GPIO_CHIP_NAME_MAX_LENGTH];
+    if ((pin >= 0) && (pin < sizeof(gPinIndex) / sizeof(gPinIndex[0]))) {
+        int32_t index = gPinIndex[pin];
+        if (gpGpioChip[index] == NULL) {
+            snprintf(gpioChipName, sizeof(gpioChipName), "%s%d",
+                     U_PORT_GPIO_CHIP_NAME_BASE, index);
+            gpGpioChip[index] = gpiod_chip_open_by_name(gpioChipName);
+        }
+        if (gpGpioChip[index] != NULL) {
+            pLine = gpiod_chip_get_line(gpGpioChip[index], (unsigned int)pin);
+        }
     }
     return pLine;
 }
@@ -104,40 +142,53 @@ static bool isOutput(struct gpiod_line *pLine)
 // Configure a GPIO.
 U_WEAK int32_t uPortGpioConfig(uPortGpioConfig_t *pConfig)
 {
-    uErrorCode_t errorCode = U_ERROR_COMMON_PLATFORM;
-    struct gpiod_line *pLine = getChip_line(pConfig->pin);
-    if (pLine != NULL) {
-        int flags = 0;
-        if ((pConfig->direction == U_PORT_GPIO_DIRECTION_OUTPUT) ||
-            (pConfig->direction == U_PORT_GPIO_DIRECTION_INPUT_OUTPUT)) {
-            // There is no difference between U_PORT_GPIO_DIRECTION_OUTPUT and
-            // U_PORT_GPIO_DIRECTION_INPUT_OUTPUT as we can always read
-            // the current output value.
-            if (pConfig->driveMode == U_PORT_GPIO_DRIVE_MODE_OPEN_DRAIN) {
-                flags = GPIOD_LINE_REQUEST_FLAG_OPEN_DRAIN;
-            }
-            // gpiod_line_request_output_flags requires an initial level value.
-            // If the pin was not configured earlier as an output we can not obtain
-            // it and hence we set it to 0.
-            int level = gpiod_line_get_value(pLine);
-            if (level < 0) {
-                level = 0;
-            }
-            releaseIfAllocated(pLine);
-            if (gpiod_line_request_output_flags(pLine, U_PORT_GPIO_CONSUMER_NAME, flags, level) == 0) {
-                errorCode = U_ERROR_COMMON_SUCCESS;
-            }
-        } else if (pConfig->direction == U_PORT_GPIO_DIRECTION_INPUT) {
-            if (pConfig->pullMode == U_PORT_GPIO_PULL_MODE_PULL_UP) {
-                flags = _GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_UP;
-            } else if (pConfig->pullMode == U_PORT_GPIO_PULL_MODE_PULL_DOWN) {
-                flags = _GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_DOWN;
-            } else if (pConfig->pullMode == U_PORT_GPIO_PULL_MODE_NONE) {
-                flags = _GPIOD_LINE_REQUEST_FLAG_BIAS_DISABLE;
-            }
-            releaseIfAllocated(pLine);
-            if (gpiod_line_request_input_flags(pLine, U_PORT_GPIO_CONSUMER_NAME, flags) == 0) {
-                errorCode = U_ERROR_COMMON_SUCCESS;
+    uErrorCode_t errorCode = U_ERROR_COMMON_INVALID_PARAMETER;
+    int32_t index;
+    if ((pConfig != NULL) &&
+        (pConfig->pin >= 0) && (pConfig->pin < sizeof(gPinIndex) / sizeof(gPinIndex[0])) &&
+        // Needs to be a signed compare or an index of -1 fails the test below
+        (pConfig->index < (int32_t) (sizeof(gpGpioChip) / sizeof(gpGpioChip[0])))) {
+        index = pConfig->index;
+        if (index < 0) {
+            index = U_PORT_GPIO_CHIP_INDEX_DEFAULT;
+        }
+        // Remember the GPIO chip index for this pin
+        gPinIndex[pConfig->pin] = index;
+        errorCode = U_ERROR_COMMON_PLATFORM;
+        struct gpiod_line *pLine = pGetChipLine(pConfig->pin);
+        if (pLine != NULL) {
+            int flags = 0;
+            if ((pConfig->direction == U_PORT_GPIO_DIRECTION_OUTPUT) ||
+                (pConfig->direction == U_PORT_GPIO_DIRECTION_INPUT_OUTPUT)) {
+                // There is no difference between U_PORT_GPIO_DIRECTION_OUTPUT and
+                // U_PORT_GPIO_DIRECTION_INPUT_OUTPUT as we can always read
+                // the current output value.
+                if (pConfig->driveMode == U_PORT_GPIO_DRIVE_MODE_OPEN_DRAIN) {
+                    flags = GPIOD_LINE_REQUEST_FLAG_OPEN_DRAIN;
+                }
+                // gpiod_line_request_output_flags requires an initial level value.
+                // If the pin was not configured earlier as an output we can not obtain
+                // it and hence we set it to 0.
+                int level = gpiod_line_get_value(pLine);
+                if (level < 0) {
+                    level = 0;
+                }
+                releaseIfAllocated(pLine);
+                if (gpiod_line_request_output_flags(pLine, U_PORT_GPIO_CONSUMER_NAME, flags, level) == 0) {
+                    errorCode = U_ERROR_COMMON_SUCCESS;
+                }
+            } else if (pConfig->direction == U_PORT_GPIO_DIRECTION_INPUT) {
+                if (pConfig->pullMode == U_PORT_GPIO_PULL_MODE_PULL_UP) {
+                    flags = _GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_UP;
+                } else if (pConfig->pullMode == U_PORT_GPIO_PULL_MODE_PULL_DOWN) {
+                    flags = _GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_DOWN;
+                } else if (pConfig->pullMode == U_PORT_GPIO_PULL_MODE_NONE) {
+                    flags = _GPIOD_LINE_REQUEST_FLAG_BIAS_DISABLE;
+                }
+                releaseIfAllocated(pLine);
+                if (gpiod_line_request_input_flags(pLine, U_PORT_GPIO_CONSUMER_NAME, flags) == 0) {
+                    errorCode = U_ERROR_COMMON_SUCCESS;
+                }
             }
         }
     }
@@ -147,11 +198,11 @@ U_WEAK int32_t uPortGpioConfig(uPortGpioConfig_t *pConfig)
 // Set the state of a GPIO.
 U_WEAK int32_t uPortGpioSet(int32_t pin, int32_t level)
 {
-    uErrorCode_t errorCode = U_ERROR_COMMON_PLATFORM;
-    struct gpiod_line *pLine = getChip_line(pin);
+    uErrorCode_t errorCode = U_ERROR_COMMON_INVALID_PARAMETER;
+    struct gpiod_line *pLine = pGetChipLine(pin);
     if (pLine != NULL) {
         // The pin may not yet have been defined as output via uPortGpioConfig.
-        int res;
+        int32_t res;
         if (isOutput(pLine)) {
             res = gpiod_line_set_value(pLine, level);
         } else {
@@ -168,8 +219,8 @@ U_WEAK int32_t uPortGpioSet(int32_t pin, int32_t level)
 // Get the state of a GPIO.
 U_WEAK int32_t uPortGpioGet(int32_t pin)
 {
-    int32_t level = (int32_t)U_ERROR_COMMON_PLATFORM;
-    struct gpiod_line *pLine = getChip_line(pin);
+    int32_t level = (int32_t)U_ERROR_COMMON_INVALID_PARAMETER;
+    struct gpiod_line *pLine = pGetChipLine(pin);
     if (pLine != NULL) {
         level = gpiod_line_get_value(pLine);
     }
