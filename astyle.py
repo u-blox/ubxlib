eb737d1bc7f91f50183aa79da60dcf90639c80ce
Alon Bar-Lev <alon.barlev@gmail.com>
2023-10-14 19:00:27 +0300

    Github, add CI action:  AStyle CI (#145)
    It is now possible to run AStyle and Doxyen checks as Github actions: many thanks to alonbl for contributing this.

--- astyle.py
+++ astyle.py
@@ -6,11 +6,18 @@
 # on all relevant files in the ubxlib directory tree, using the standard ubxlib astyle.cfg
 
 import subprocess
+import sys
 
-with subprocess.Popen("astyle --options=astyle.cfg --suffix=none --verbose " \
-                      "--errors-to-stdout --recursive \"*.c,*.h,*.cpp,*.hpp\"",
+with subprocess.Popen(["astyle", "--options=astyle.cfg", "--suffix=none", "--verbose",
+                      "--errors-to-stdout", "--recursive", "*.c,*.h,*.cpp,*.hpp"],
                       stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                       universal_newlines=True) as astyle:
+    fail = False
     output = astyle.communicate()
     for line in output:
-        print (line, end=" ")
+        if line.startswith("Formatted"):
+            fail = True
+        print (line, end="")
+    if astyle.returncode != 0:
+        fail = True
+    sys.exit(1 if fail else 0)
