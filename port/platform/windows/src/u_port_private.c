94a15fe13836b2e6dfb2a6cd75cf6633023464a1
Rob Meades <RobMeades@users.noreply.github.com>
2024-01-23 11:45:32 +0000

    Windows platform change only: fix critical section hole. (#1076)
    The functions uPortEnterCritical()/uPortExitCritical(), which suspend/resume all but the current task, use a list of tasks maintained by uPortTaskCreate()/uPortTaskDelete() and all of these functions individually lock a single gMutexThread to ensure re-entrancy.
    
    However, very occasionally, the function uPortPrivateEnterCritical() asserts that it is already in a critical section, even though it cannot be, pointing to a flaw in the logic.
    
    The flaw is that it is possible for uPortTaskCreate() or uPortTaskDelete() to be called DURING a critical section, i.e. after uPortEnterCritical() has returned and before uPortExitCritical() has been called; a task created at such a time will NOT be suspended, effectively breaking the critical section; not sure what deleting a task during that time would do but it is probably not good.
    
    To fix this, uPortEnterCritical() now locks gMutexThread but does NOT unlock it, uPortExitCritical() performing the unlock part; this fixes the hole.

--- port/platform/windows/src/u_port_private.c
+++ port/platform/windows/src/u_port_private.c
@@ -76,7 +76,7 @@ uPortTaskHandle_t gMutexDebugWatchdogTaskHandle = NULL;
  * uPortPrivateExitCritical() code, we need to go "around" the
  * mutex debug code by using the macro below.
  */
-#define U_PORT_MUTEX_LOCK_NO_MUTEX_DEBUG(x)      { U_ASSERT(_uPortMutexLock(((uPortPrivatePartialMutexInfo_t *) x)->handle) == 0)
+#define U_PORT_MUTEX_LOCK_NO_MUTEX_DEBUG(x)      U_ASSERT(_uPortMutexLock(((uPortPrivatePartialMutexInfo_t *) x)->handle) == 0)
 
 /** Bypass mutex debug for unlock: On Windows the functions
  * uPortPrivateEnterCritical()/uPortPrivateExitCritical() are
@@ -89,15 +89,15 @@ uPortTaskHandle_t gMutexDebugWatchdogTaskHandle = NULL;
  * uPortPrivateExitCritical() code, we need to go "around" the
  * mutex debug code by using the macro below.
  */
-#define U_PORT_MUTEX_UNLOCK_NO_MUTEX_DEBUG(x)    } U_ASSERT(_uPortMutexUnlock(((uPortPrivatePartialMutexInfo_t *) x)->handle) == 0)
+#define U_PORT_MUTEX_UNLOCK_NO_MUTEX_DEBUG(x)    U_ASSERT(_uPortMutexUnlock(((uPortPrivatePartialMutexInfo_t *) x)->handle) == 0)
 #else
 /** Mutex lock helper macro.
  */
-#define U_PORT_MUTEX_LOCK_NO_MUTEX_DEBUG       U_PORT_MUTEX_LOCK
+#define U_PORT_MUTEX_LOCK_NO_MUTEX_DEBUG(x)      uPortMutexLock(x)
 
 /** Mutex unlock helper macro.
  */
-#define U_PORT_MUTEX_UNLOCK_NO_MUTEX_DEBUG     U_PORT_MUTEX_UNLOCK
+#define U_PORT_MUTEX_UNLOCK_NO_MUTEX_DEBUG(x)    uPortMutexUnlock(x)
 #endif
 
 /* ----------------------------------------------------------------
@@ -542,6 +542,7 @@ int32_t uPortPrivateEnterCritical()
     if (gMutexThread != NULL) {
         errorCode = (int32_t) U_ERROR_COMMON_SUCCESS;
 
+        // Lock gMutexThread, to protect the list of threads
         U_PORT_MUTEX_LOCK_NO_MUTEX_DEBUG(gMutexThread);
 
         U_ASSERT(!gInCriticalSection);
@@ -573,7 +574,7 @@ int32_t uPortPrivateEnterCritical()
 
         gInCriticalSection = true;
 
-        U_PORT_MUTEX_UNLOCK_NO_MUTEX_DEBUG(gMutexThread);
+        // DO NOT UNLOCK gMutexThread: uPortPrivateExitCritical() will do that
     }
 
     return (int32_t) errorCode;
@@ -588,8 +589,6 @@ int32_t uPortPrivateExitCritical()
 
     if (gMutexThread != NULL) {
         errorCode = (int32_t) U_ERROR_COMMON_SUCCESS;
-
-        U_PORT_MUTEX_LOCK_NO_MUTEX_DEBUG(gMutexThread);
 
         U_ASSERT(gInCriticalSection);
 
@@ -616,6 +615,7 @@ int32_t uPortPrivateExitCritical()
 
         gInCriticalSection = false;
 
+        // NOW we can unlock gMutexThread
         U_PORT_MUTEX_UNLOCK_NO_MUTEX_DEBUG(gMutexThread);
     }
 
