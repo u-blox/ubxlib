u-blox/ubxlib

commit 6d12d9fcd4827d384b2774c9d9e2a6ad2688447e

PORT API I2C CHANGE, fixing Zephyr for STM32: uPortI2cControllerExchange() replaces uPortI2cControllerSend()/uPortI2cControllerSendReceive(). (#1105)
The port API for I2C had functions uPortI2cControllerSend(), where the stop bit could optionally be omitted, and uPortI2cControllerSendReceive(), for normal communications.  Sending without a stop bit is required to allow the register address within a u-blox GNSS chip to be set, i.e. a write of 0xFD with no stop bit causes the register address in the GNSS chip to be set to 0xFD, and that is the register address from which the number of bytes the GNSS chip has ready for transmission can be read.

However, there is driver software out there (e.g. the STM32 I2C drivers within Zephyr, and potentially Zephyr in general aside from the Nordic I2C drivers) which FORCES a stop bit at the end of each I2C transfer.

In order to be maximally compatible with all platforms, uPortI2cControllerSend()/uPortI2cControllerSendReceive() is now replaced with a single uPortI2cControllerExchange() function, with the option of leaving off the stop bit between the send and the receive.  The uPortI2cControllerSend() and uPortI2cControllerSendReceive() functions are deprecated but remain for now: a weakly-linked implementation of uPortI2cControllerExchange() is provided which calls through to uPortI2cControllerSend()/uPortI2cControllerSendReceive() to ensure backwards compatibility.

For Zephyr, uPortI2cControllerExchange() is implemented such that I2C transactions work with STM32 MCUs as well as nRF52/nRF53.

Our thanks to AarC10/Naquino14 and djfurie for highlighting this issue.

P.S. the number of I2C ports supported for Zephyr is also increased from 2 to 4 to match the capabilities of the STM32 family.

port/api/u_port_i2c.h
@@ -258,6 +258,85 @@ int32_t uPortI2cGetMaxSegmentSize(int32_t handle);
  * Note that the NRF52 and NRF53 chips require all buffers to
  * be in RAM.
  *
+ * Note: where this function is not implemented a weakly-linked
+ * version will currently do the following:
+ *
+ * (a) call uPortI2cControllerSend() if pReceive is NULL or
+ *     noInterveningStop is true,
+ * (b) call uPortI2cControllerSend() followed by
+ *     uPortI2cControllerSendReceive() if pReceive is non-NULL
+ *     and noInterveningStop is true,
+ * (c) otherwise call uPortI2cControllerSendReceive().
+ *
+ * This weakly-linked function will be removed when the deprecated
+ * uPortI2cControllerSend()/uPortI2cControllerSendReceive() functions
+ * are removed.
+ *
+ * Note also that the uPortI2cSetTimeout() (or the equivalent set
+ * by a platform at compile-time) applies for the whole of this
+ * transaction, i.e. the peripheral must begin responding within
+ * that time; if you wish to allow the peripheral longer to respond
+ * you should take control of the time allowed yourself by calling
+ * uPortI2cControllerExchange() twice, once with only a send buffer
+ * and again with only a receive buffer.
+ *
+ * @param handle            the handle of the I2C instance.
+ * @param address           the I2C address to send to; only the lower
+ *                          7 bits are used unless the platform supports
+ *                          10-bit addressing.  Note that the NRF5 SDK,
+ *                          and hence Zephyr on NRF52/53 (which uses the NRF5
+ *                          SDK under the hood) does not support 10-bit
+ *                          addressing and, in any case, we've not yet found
+ *                          a device that supports 10-bit addressing to test
+ *                          against.
+ * @param[in] pSend         a pointer to the data to send, use NULL
+ *                          if only receive is required; setting this and
+ *                          pReceive to NULL will return success only if a
+ *                          device with the given address is present on the I2C
+ *                          bus; however note that the NRFX drivers used on nRF52
+ *                          and nRF53 by NRF-SDK and Zephyr don't support
+ *                          sending only the address, data must follow.
+ * @param bytesToSend       the number of bytes to send, must be zero if pSend
+ *                          is NULL.
+ * @param[out] pReceive     a pointer to a buffer in which to store received
+ *                          data; use NULL if only send is required.
+ * @param bytesToReceive    the size of buffer pointed to by pReceive, must
+ *                          be zero if pReceive is NULL.
+ * @param noInterveningStop if true then no stop is sent between the send
+ *                          and the receive; this is useful for devices such
+ *                          as EEPROMs or, in certain situations, u-blox GNSS
+ *                          modules, which allow writing of a memory address
+ *                          byte or bytes, followed by no stop bit; the data
+ *                          from that memory address may then be received
+ *                          into pReceive.  This is sometimes called using a
+ *                          "repeated start bit".  Note that if pReceive
+ *                          is NULL, depending on the platform, this _may_
+ *                          be ignored and a stop bit added in any case;
+ *                          e.g. the STM32 drivers within Zephyr will do this;
+ *                          they require a stop bit at the end of an I2C
+ *                          transaction.
+ * @return                  if pReceive is not NULL the number of bytes
+ *                          received or negative error code; if pReceive is
+ *                          NULL then zero on success else negative error code.
+ *                          Note that the underlying platform drivers often
+ *                          do not report the number of bytes received and
+ *                          hence the return value may just be either an
+ *                          error code or bytesToReceive copied back to you.
+ */
+int32_t uPortI2cControllerExchange(int32_t handle, uint16_t address,
+                                   const char *pSend, size_t bytesToSend,
+                                   char *pReceive, size_t bytesToReceive,
+                                   bool noInterveningStop);
+
+/** \deprecated this function is deprecated, please use
+ * uPortI2cControllerExchange() instead; if you are making your own
+ * I2C port, please implement uPortI2cControllerExchange() and
+ * not this function.
+ *
+ * Send and/or receive over the I2C interface as a controller.
+ * Note that the NRF52 and NRF53 chips require all buffers to
+ * be in RAM.
+ *
  * Note that the uPortI2cSetTimeout() (or the equivalent set
  * by a platform at compile-time) applies for the whole of this
  * transaction, i.e. the peripheral must begin responding within
@@ -275,7 +354,7 @@ int32_t uPortI2cGetMaxSegmentSize(int32_t handle);
  *                       addressing and, in any case, we've not yet found
  *                       a device that supports 10-bit addressing to test
  *                       against.
- * @param pSend          a pointer to the data to send, use NULL
+ * @param[in] pSend      a pointer to the data to send, use NULL
  *                       if only receive is required.  This function
  *                       will do nothing, and return success, if both
  *                       pSend and pReceive are NULL; if you want to do
@@ -284,7 +363,7 @@ int32_t uPortI2cGetMaxSegmentSize(int32_t handle);
  *                       though note that not all platforms support this.
  * @param bytesToSend    the number of bytes to send, must be zero if pSend
  *                       is NULL.
- * @param pReceive       a pointer to a buffer in which to store received
+ * @param[out] pReceive  a pointer to a buffer in which to store received
  *                       data; use NULL if only send is required.
  * @param bytesToReceive the size of buffer pointed to by pReceive, must
  *                       be zero if pReceive is NULL.
@@ -300,7 +379,12 @@ int32_t uPortI2cControllerSendReceive(int32_t handle, uint16_t address,
                                       const char *pSend, size_t bytesToSend,
                                       char *pReceive, size_t bytesToReceive);
 
-/** Perform just a send over the I2C interface as a controller, with the
+/** \deprecated this function is deprecated, please use
+ * uPortI2cControllerExchange() instead; if you are making your own
+ * I2C port, please implement uPortI2cControllerExchange() and
+ * not this function.
+ *
+ * Perform just a send over the I2C interface as a controller, with the
  * option of omitting the stop marker on the end.
  * Note that the NRF52 and NRF53 chips require the buffer to be in RAM.
  *
@@ -313,7 +397,7 @@ int32_t uPortI2cControllerSendReceive(int32_t handle, uint16_t address,
  *                       addressing and, in any case, we've not yet found
  *                       a device that supports 10-bit addressing to test
  *                       against.
- * @param pSend          a pointer to the data to send; setting this to
+ * @param[in] pSend      a pointer to the data to send; setting this to
  *                       NULL will return success only if a device with
  *                       the given address is present on the I2C bus;
  *                       however note that the NRFX drivers used on nRF52

gnss/src/u_gnss_msg.c
@@ -528,9 +528,9 @@ int32_t uGnssMsgSend(uDeviceHandle_t gnssHandle, const char *pBuffer, size_t siz
                     }
                     break;
                     case U_GNSS_PRIVATE_STREAM_TYPE_I2C: {
-                        errorCodeOrLength = uPortI2cControllerSend(pInstance->transportHandle.i2c,
-                                                                   pInstance->i2cAddress,
-                                                                   pBuffer, size, false);
+                        errorCodeOrLength = uPortI2cControllerExchange(pInstance->transportHandle.i2c,
+                                                                       pInstance->i2cAddress,
+                                                                       pBuffer, size, NULL, 0, false);
                         if (errorCodeOrLength == 0) {
                             errorCodeOrLength = (int32_t) size;
                         }

gnss/src/u_gnss_private.c
@@ -373,9 +373,10 @@ static int32_t sendMessageStream(uGnssPrivateInstance_t *pInstance,
         }
         break;
         case U_GNSS_PRIVATE_STREAM_TYPE_I2C: {
-            errorCodeOrSentLength = uPortI2cControllerSend(pInstance->transportHandle.i2c,
-                                                           pInstance->i2cAddress,
-                                                           pMessage, messageLengthBytes, false);
+            errorCodeOrSentLength = uPortI2cControllerExchange(pInstance->transportHandle.i2c,
+                                                               pInstance->i2cAddress,
+                                                               pMessage, messageLengthBytes,
+                                                               NULL, 0, false);
             if (errorCodeOrSentLength == 0) {
                 errorCodeOrSentLength = messageLengthBytes;
             }
@@ -1981,7 +1982,7 @@ int32_t uGnssPrivateStreamGetReceiveSize(uGnssPrivateInstance_t *pInstance)
 {
     int32_t errorCodeOrReceiveSize = (int32_t) U_ERROR_COMMON_INVALID_PARAMETER;
     int32_t privateStreamTypeOrError;
-    char buffer[2];
+    char buffer[2] = {0};
 
     if (pInstance != NULL) {
         privateStreamTypeOrError = uGnssPrivateGetStreamType(pInstance->transportType);
@@ -1998,16 +1999,13 @@ int32_t uGnssPrivateStreamGetReceiveSize(uGnssPrivateInstance_t *pInstance)
                 // 0xFD, with no stop bit, and then a read request for two bytes
                 // should get us the [big-endian] length
                 buffer[0] = 0xFD;
-                errorCodeOrReceiveSize = uPortI2cControllerSend(pInstance->transportHandle.i2c,
-                                                                (uint16_t) i2cAddress,
-                                                                buffer, 1, true);
-                if (errorCodeOrReceiveSize == 0) {
-                    errorCodeOrReceiveSize = uPortI2cControllerSendReceive(pInstance->transportHandle.i2c,
-                                                                           (uint16_t) i2cAddress,
-                                                                           NULL, 0, buffer, sizeof(buffer));
-                    if (errorCodeOrReceiveSize == sizeof(buffer)) {
-                        errorCodeOrReceiveSize = (int32_t) ((((uint32_t) buffer[0]) << 8) + (uint32_t) buffer[1]);
-                    }
+                errorCodeOrReceiveSize = uPortI2cControllerExchange(pInstance->transportHandle.i2c,
+                                                                    (uint16_t) i2cAddress,
+                                                                    buffer, 1,
+                                                                    buffer, sizeof(buffer),
+                                                                    true);
+                if (errorCodeOrReceiveSize == sizeof(buffer)) {
+                    errorCodeOrReceiveSize = (int32_t) ((((uint32_t) buffer[0]) << 8) + (uint32_t) buffer[1]);
                 }
             }
             break;
@@ -2186,11 +2184,11 @@ int32_t uGnssPrivateStreamFillRingBuffer(uGnssPrivateInstance_t *pInstance,
                             // the I2C buffer is effectively on the GNSS chip and I2C drivers
                             // often don't say how much they've read, just giving us back
                             // the number we asked for on a successful read
-                            receiveSize = uPortI2cControllerSendReceive(pInstance->transportHandle.i2c,
-                                                                        pInstance->i2cAddress,
-                                                                        NULL, 0,
-                                                                        pTemporaryBuffer,
-                                                                        receiveSize);
+                            receiveSize = uPortI2cControllerExchange(pInstance->transportHandle.i2c,
+                                                                     pInstance->i2cAddress,
+                                                                     NULL, 0,
+                                                                     pTemporaryBuffer,
+                                                                     receiveSize, false);
                             break;
                         case U_GNSS_PRIVATE_STREAM_TYPE_SPI:
                             // For the SPI case, we need to pull the data that was

commit e96516309e8a05e9ca910931e511b00770a0a0a4

Mostly test change only: add Zephyr-STM32U575ZI instance (32). (#1108)
A Nucleo U575ZI board is added, with an M9 GNSS device connected over I2C; this serves to regression-test the I2C change for Zephyr introduced in commit 6d12d9fcd4827d384b2774c9d9e2a6ad2688447e.

port/api/u_port_i2c.h
@@ -225,7 +225,8 @@ int32_t uPortI2cGetTimeout(int32_t handle);
 /** Set the maximum segment size for an I2C transfer in both
  * directions; this should be used only on chipsets where the HW
  * interface is limited (e.g. nRF52832, which has a maximum DMA
- * size of 256 for I2C): any transfers above this size will be
+ * size of 256 for I2C, STM32 on Zephyr which has a similar (though
+ * not DMA related) limitation): any transfers above this size will be
  * segmented into N transfers of no more than this size.  If this is
  * not called no segmentation will be applied.  Where this is not
  * supported a weakly-linked function will return

port/platform/common/automation/53_dut.rules
@@ -33,3 +33,6 @@ SUBSYSTEM=="tty", ATTRS{idVendor}=="1366", ATTRS{idProduct}=="1055", ENV{ID_SEGG
 SUBSYSTEMS=="usb", ENV{ID_STLINK_V2_1_STANDALONE_INTERFACE_NUMBER}="$attr{bInterfaceNumber}", MODE:="0666", SYMLINK+="stlink_v2_1_standalone"
 SUBSYSTEM=="tty", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", ENV{ID_STLINK_V2_1_STANDALONE_INTERFACE_NUMBER}=="07", MODE:="0666", SYMLINK+="stlink_v2_1_standalone_uart"
 
+# STLink V3 debugger, as used on Nucleo U575ZI boards
+SUBSYSTEMS=="usb", ENV{ID_STLINK_V3_INTERFACE_NUMBER}="$attr{bInterfaceNumber}", MODE:="0666", SYMLINK+="stlink_v3"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374e", ENV{ID_STLINK_V3_INTERFACE_NUMBER}=="07", MODE:="0666", SYMLINK+="stlink_v3"

port/platform/common/automation/scripts/u_connection.py
@@ -76,7 +76,9 @@ CONNECTION_LIST = [
     # Instance 30, STM32F4, NORA-W36, LARA-R6
     {"lock": None, "id": 30, **u_settings.CONNECTION_INSTANCE_30},
     # Instance 31, STM32, native Zephyr
-    {"lock": None, "id": 31, **u_settings.CONNECTION_INSTANCE_31}
+    {"lock": None, "id": 31, **u_settings.CONNECTION_INSTANCE_31},
+    # Instance 32, STM32, native Zephyr
+    {"lock": None, "id": 32, **u_settings.CONNECTION_INSTANCE_32}
 ]
 
 def init_locks(manager):

port/platform/common/automation/scripts/u_settings.py
@@ -158,7 +158,13 @@ __DEFAULT_SETTINGS["CONNECTION_INSTANCE_29" + __SETTINGS_POSTFIX_AGENT_SPECIFIC]
 __DEFAULT_SETTINGS["CONNECTION_INSTANCE_30" + __SETTINGS_POSTFIX_AGENT_SPECIFIC] = \
     {"serial_port": "/dev/stlink_v2_1", "debugger": "0674FF575377524867061450"}
 __DEFAULT_SETTINGS["CONNECTION_INSTANCE_31" + __SETTINGS_POSTFIX_AGENT_SPECIFIC] = \
-    {"serial_port": "/dev/serial/by-id/usb-STMicroelectronics_STM32_STLink_066DFF313937424757082031-if02", "debugger": "066DFF313937424757082031"}
+    {"serial_port": ("/dev/serial/by-id/"
+                     "usb-STMicroelectronics_STM32_STLink_066DFF313937424757082031-if02"),
+     "debugger": "066DFF313937424757082031"}
+__DEFAULT_SETTINGS["CONNECTION_INSTANCE_32" + __SETTINGS_POSTFIX_AGENT_SPECIFIC] = \
+    {"serial_port": ("/dev/serial/by-id/"
+                     "usb-STMicroelectronics_STLINK-V3_004800273532510F31333430-if02"),
+     "debugger": "004800273532510F31333430"}
 # u_data.py
 __DEFAULT_SETTINGS["DATA_FILE"] = "DATABASE.md"
 __DEFAULT_SETTINGS["CELLULAR_MODULE_TYPE_PREFIX"] = "U_CELL_MODULE_TYPE_"

port/platform/common/automation/tasks/zephyr_native.py
@@ -17,7 +17,8 @@ DEFAULT_OUTPUT_NAME = f"runner"
 DEFAULT_BUILD_DIR = os.path.join("_build", "zephyr_native")
 
 BOARD_NAME_TO_MCU = {
-    "nucleo_f767zi": "stm32"
+    "nucleo_f767zi": "stm32",
+    "nucleo_u575zi_q": "stm32"
 }
 
 def posix_path(path):
@@ -67,7 +68,7 @@ def get_mcu(board_name):
     """Get the MCU name for the given board_name"""
     mcu = BOARD_NAME_TO_MCU.get(board_name)
     if not mcu:
-        print(f"Board {board_name} not found in list of known board names")
+        print(f"Board {board_name} not found in list of known board names in automation script zephyr_native.py")
 
     return mcu
 
@@ -165,7 +166,7 @@ def flash(ctx, board_name, debugger_serial="", output_name=DEFAULT_OUTPUT_NAME,
     build_dir = os.path.abspath(os.path.join(build_dir, output_name))
     if debugger_serial != "":
         debugger_serial = f"--serial {debugger_serial}"
-    ctx.run(f'{ctx.zephyr_pre_command}west flash --skip-rebuild -d {build_dir} {debugger_serial}', hide=False)
+    ctx.run(f'{ctx.zephyr_pre_command}west flash --skip-rebuild -d {build_dir} --runner openocd {debugger_serial}', hide=False)
 
 @task(
     pre=[check_installation],

port/platform/common/automation/u_packages.yml
@@ -24,14 +24,14 @@ arm_embedded_gcc:
 
 openocd:
     type: archive
-    version: v0.11.0-3
+    version: v0.12.0-2
     skip_first_subdir: True
     windows:
-        url: https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.11.0-3/xpack-openocd-0.11.0-3-win32-x64.zip
+        url: https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.12.0-2/xpack-openocd-0.12.0-2-win32-x64.zip
     linux:
-        url: https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.11.0-3/xpack-openocd-0.11.0-3-linux-x64.tar.gz
+        url: https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.12.0-2/xpack-openocd-0.12.0-2-linux-x64.tar.gz
     linux_arm:
-        url: https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.11.0-3/xpack-openocd-0.11.0-3-linux-arm64.tar.gz
+        url: https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.12.0-2/xpack-openocd-0.12.0-2-linux-arm64.tar.gz
     add_to_path: bin
 
 nrf5sdk:

port/platform/zephyr/cfg/u_cfg_app_platform_specific.h
@@ -168,7 +168,7 @@
 #endif
 
 #ifndef U_CFG_APP_PIN_CELL_PWR_ON
-# ifdef CONFIG_BOARD_NUCLEO_F767ZI
+# if defined(CONFIG_BOARD_NUCLEO_F767ZI) || defined(CONFIG_BOARD_NUCLEO_U575ZI_Q)
 /** The STM32 GPIO output that is connected to the PWR_ON
  * pin of the cellular module.
  */
@@ -303,7 +303,8 @@
 #ifndef U_CFG_APP_GNSS_I2C
 /** The I2C HW block to use inside the NRF5x chip to communicate
  * with a GNSS module.  If this is required, please use number 1
- * as that is the one that the NRFx drivers used by Zephyr supports.
+ * as that is the one that the NRFx drivers used by Zephyr supports
+ * and it is also the default for the Nucleo_U575ZI_q board.
  * You will also need to set the following in your prj.cnf file:
  *
  * CONFIG_I2C=y

port/platform/zephyr/cfg/u_cfg_test_platform_specific.h
@@ -143,6 +143,10 @@
 #include "u_cfg_test_platform_specific_nucleo_f767zi.h"
 #endif
 
+#ifdef CONFIG_BOARD_NUCLEO_U575ZI_Q
+#include "u_cfg_test_platform_specific_nucleo_u575zi_q.h"
+#endif
+
 #ifdef CONFIG_BOARD_NATIVE_POSIX
 #include "u_cfg_test_platform_specific_native_posix.h"
 #endif

port/platform/zephyr/cfg/u_cfg_test_platform_specific_nucleo_u575zi_q.h
@@ -1,0 +1,53 @@
+/*
+ * Copyright 2019-2024 u-blox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _U_CFG_TEST_PLATFORM_SPECIFIC_NUCLEO_F767ZI_H_
+#define _U_CFG_TEST_PLATFORM_SPECIFIC_NUCLEO_F767ZI_H_
+
+/** Pin A for GPIO testing: will be used as an output and
+ * must be connected to pin B via a 1k resistor.
+ */
+#ifndef U_CFG_TEST_PIN_A
+# define U_CFG_TEST_PIN_A         -1
+#endif
+
+/** Pin B for GPIO testing: will be used as both an input and
+ * and open drain output and must be connected both to pin A via
+ * a 1k resistor and directly to pin C.
+ */
+#ifndef U_CFG_TEST_PIN_B
+# define U_CFG_TEST_PIN_B         -1
+#endif
+
+/** Pin C for GPIO testing: must be connected to pin B,
+ * will be used as an input only.
+ */
+#ifndef U_CFG_TEST_PIN_C
+# define U_CFG_TEST_PIN_C         -1
+#endif
+
+/** Reset pin for a GNSS module, required when such a module is
+ * connected via I2C and needs resetting before it is used for
+ * testing the I2C port layer; should be connected to the RESET_N
+ * pin of the GNSS module.
+ */
+#ifndef U_CFG_TEST_PIN_GNSS_RESET_N
+# define U_CFG_TEST_PIN_GNSS_RESET_N   -1
+#endif
+
+#endif // _U_CFG_TEST_PLATFORM_SPECIFIC_NUCLEO_F767ZI_H_
+
+// End of file

port/platform/zephyr/runner_stm32/boards/nucleo_u575zi_q.overlay
@@ -1,0 +1,47 @@
+/ { // This portion in the "root" of the device tree
+
+    // A GNSS device connected to I2C1
+    cfg-device-gnss {
+        compatible = "u-blox,ubxlib-device-gnss";
+        status = "okay";
+        transport-type = "i2c1";
+        // There is an I2C segment size limitation on some STM32
+        // devices so limit to 255 byte segments to be on the safe
+        // side
+        i2c-max-segment-size = <255>;
+        // Allow the GNSS module type to be determined automatically
+    };
+
+    chosen {
+            // Only required if U_CFG_PPP_ENABLE is enabled
+            zephyr,ppp-uart = &uart99;
+    };
+
+    // Only required if U_CFG_PPP_ENABLE is enabled
+    // This creates instance 99 of a uart that we will give to zephyr,ppp-uart
+    uart99: uart-ppp@8000 { // The "8000" here is irrelevant but required for Zephyr to work
+        compatible = "u-blox,uart-ppp"; // The important part: this is an instance of u-blox,uart-ppp
+        reg = <0x8000 0x100>; // This is irrelevant but required for Zephyr to work
+        status = "okay"; // Zephyr boiler-plate
+    };
+};
+
+// Enable DMA (a) because STM32 serial ports
+// have only a one-byte buffer, so will lose
+// characters without it, and, (b) because we
+// need an asynchronous UART for PPP and the
+// device tree configuration on STM32 won't
+// allow that unless at least one thing has
+// DMA enabled
+// Guidance from table 136 of reference manual
+// RM0456 and
+// https://github.com/zephyrproject-rtos/zephyr/blob/main/dts/bindings/dma/st%2Cstm32u5-dma.yaml
+// Also, make console logging use DMA for speed
+&usart3 {
+    dmas = <&gpdma1 0 29 STM32_DMA_PERIPH_TX
+            &gpdma1 1 28 STM32_DMA_PERIPH_RX>;
+    dma-names = "tx", "rx";
+};
+&gpdma1 {
+    status = "okay";
+};

port/platform/zephyr/src/u_port_board_cfg.c
@@ -1239,7 +1239,10 @@ static void cfgShortRange(uDeviceCfg_t *pCfg, int32_t index)
 #if DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_ble) ||       \
     DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_cellular) ||  \
     DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_gnss) ||      \
-    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_wifi)
+    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_wifi) ||      \
+    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_cellular) ||   \
+    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_gnss) ||       \
+    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_short_range)
 
 // For the given device instance, return the index of it
 // in the various arrays above.
@@ -1349,6 +1352,19 @@ static int32_t getNetworkIndex(int32_t deviceIndex,
     return index;
 }
 
+#endif // #if DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_ble) ||
+//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_cellular) ||
+//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_gnss) ||
+//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_wifi)
+//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_cellular) ||
+//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_gnss) ||
+//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_short_range)
+
+#if DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_ble) ||     \
+    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_cellular) || \
+    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_gnss) ||    \
+    DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_wifi)
+
 // Get the int32_t enum value for a string.
 static bool getEnum(const char *pString,
                     uPortBoardCfgStringToEnum_t *pTableStringToEnum,
@@ -1374,7 +1390,7 @@ static bool getEnum(const char *pString,
 }
 
 #endif // #if DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_ble) ||
-//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_cellular) ||
+//            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_device_cellular) ||
 //            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_gnss) ||
 //            DT_HAS_COMPAT_STATUS_OKAY(u_blox_ubxlib_network_wifi)
 