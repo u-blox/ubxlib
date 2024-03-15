u-blox/ubxlib

commit 554d008566f9766e2732c66e76872f3bf1c9369f

Port API additions: better resource monitoring/checking. (#980)
In order to monitor resource usage during testing, new functions are added to the port API: uPortHeapAllocCount(), uPortOsResourceAllocCount(), uPortUartResourceAllocCount(), uPortI2cResourceAllocCount() and uPortSpiResourceAllocCount().

The new functions are used ONLY by the test code; they allow reliable checking of the use of memory/resources (see uTestResourceCheck() in /port/platform/common/test) without having to account for the oddities of each platform's heap allocator.

If you have YOUR OWN PORT there is NO NEED to implement these functions: weak implementations (returning zero, which will always pass all tests) are provided in the new file u_port_resource.c and the existing file u_port_heap.c. u_port_resource.c is brought in by ubxlib.cmake or ubxlib.mk; if you are not using either of those, please bring the file in yourself.

One minor bug fix is included: the task used for asynchronous GNSS message receive (see uGnssMsgReceiveStart() / uGnssMsgReceiveStop()) was being deleted more than once; this likely would have had no adverse effect.

Note: as part of this change a #define U_PORT_OS_DEBUG_PRINT is introduced which may be used [with care!] to print allocations/deallocations of OS resources.
Note: the sockets API gets a new function, uSockFree() which releases a mutex (one that would never normally be free'd, for thread-safety reasons) so that the sums work out; this function is used only by the test code and should not be used by an application.
Note: the original heap-checking code remains in place in the tests: the two mechanisms will be run in parallel for a while, later the old heap checking code may be removed from the tests.

port/platform/common/test_util/u_test_util_resource_check.c
@@ -1,0 +1,177 @@
+/*
+ * Copyright 2019-2023 u-blox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @file
+ * @brief  Functions to check for leakage of heap, OS resources (tasks etc.)
+ * and transports (UARTs etc.).
+ */
+
+#ifdef U_CFG_OVERRIDE
+# include "u_cfg_override.h" // For a customer's configuration override
+#endif
+
+#include "stddef.h"    // NULL, size_t etc.
+#include "stdint.h"    // int32_t etc.
+#include "stdbool.h"
+
+#include "u_cfg_sw.h"
+#include "u_cfg_os_platform_specific.h"  // For #define U_CFG_OS_CLIB_LEAKS
+#include "u_cfg_app_platform_specific.h"
+#include "u_cfg_test_platform_specific.h"
+
+#include "u_error_common.h"
+
+#include "u_port.h"
+#include "u_port_os.h"
+#include "u_port_heap.h"
+#include "u_port_debug.h"
+#include "u_port_uart.h"
+#include "u_port_i2c.h"
+#include "u_port_spi.h"
+
+#ifdef U_CFG_TEST_ENABLE_INACTIVITY_DETECTOR
+#include "u_debug_utils.h"
+#endif
+#ifdef U_CFG_MUTEX_DEBUG
+#include "u_mutex_debug.h"
+#endif
+
+#include "u_test_util_resource_check.h"
+
+/* ----------------------------------------------------------------
+ * COMPILE-TIME MACROS
+ * -------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------
+ * TYPES
+ * -------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------
+ * VARIABLES
+ * -------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------
+ * STATIC FUNCTIONS
+ * -------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------
+ * PUBLIC FUNCTIONS
+ * -------------------------------------------------------------- */
+
+// Check that resources are within limits and have been cleaned up.
+bool uTestUtilResourceCheck(const char *pPrefix,
+                            const char *pErrorMarker,
+                            bool printIt)
+{
+    bool resourcesClean = true;
+    int32_t x;
+    int32_t osShouldBeOutstanding = 0;
+
+    if (pPrefix == NULL) {
+        pPrefix = "";
+    }
+
+    if (pErrorMarker == NULL) {
+        pErrorMarker = "";
+    }
+
+#if defined(U_CFG_TEST_ENABLE_INACTIVITY_DETECTOR) && !defined(ARDUINO)
+    osShouldBeOutstanding += U_DEBUG_UTILS_OS_RESOURCES;
+#endif
+#ifdef U_CFG_MUTEX_DEBUG
+    osShouldBeOutstanding += U_MUTEX_DEBUG_OS_RESOURCES;
+#endif
+    osShouldBeOutstanding += U_PORT_HEAP_MONITOR_OS_RESOURCES;
+
+    // Check main task stack against our limit
+    x = uPortTaskStackMinFree(NULL);
+    if (x != (int32_t) U_ERROR_COMMON_NOT_SUPPORTED) {
+        if (x < U_CFG_TEST_OS_MAIN_TASK_MIN_FREE_STACK_BYTES) {
+            resourcesClean = false;
+        }
+        if (printIt) {
+            uPortLog("%s%smain task stack had a minimum of %d byte(s) free"
+                     " (minimum is %d).\n", pPrefix,
+                     resourcesClean ? "" : pErrorMarker,
+                     x, U_CFG_TEST_OS_MAIN_TASK_MIN_FREE_STACK_BYTES);
+        }
+    }
+
+    // Check all-time heap usage against our limit
+    x = uPortGetHeapMinFree();
+    if (x >= 0) {
+        if (x < U_CFG_TEST_HEAP_MIN_FREE_BYTES) {
+            resourcesClean = false;
+        }
+        if (printIt) {
+            uPortLog("%s%sheap had a minimum of %d byte(s) free"
+                     " (minimum is %d).\n", pPrefix,
+                     resourcesClean ? "" : pErrorMarker,
+                     x, U_CFG_TEST_HEAP_MIN_FREE_BYTES);
+        }
+    }
+
+    // Check that all heap pUPortMalloc()s have uPortFree()s
+    x = uPortHeapAllocCount();
+    if (x > 0) {
+        if (printIt) {
+            uPortLog("%s%s%d outstanding call(s) to pUPortMalloc().\n",
+                     pPrefix, pErrorMarker, x);
+            uPortHeapDump(pPrefix);
+        }
+        resourcesClean = false;
+    }
+
+    // Check that all OS resources have been free'd
+    x = uPortOsResourceAllocCount();
+    if (x != osShouldBeOutstanding) {
+        if (printIt) {
+            uPortLog("%s%sexpected %d outstanding OS resource(s) (tasks etc.)"
+                     " but got %d; they might yet be cleaned up.\n",
+                     pPrefix, pErrorMarker, osShouldBeOutstanding, x);
+        }
+        resourcesClean = false;
+    }
+    x = uPortUartResourceAllocCount();
+    if (x > 0) {
+        if (printIt) {
+            uPortLog("%s%s%d UART resource(s) outstanding.\n", pPrefix, pErrorMarker, x);
+        }
+        resourcesClean = false;
+    }
+    x = uPortI2cResourceAllocCount();
+    if (x > 0) {
+        if (printIt) {
+            uPortLog("%s%s%d I2C resource(s) outstanding.\n", pPrefix, pErrorMarker, x);
+        }
+        resourcesClean = false;
+    }
+    x = uPortSpiResourceAllocCount();
+    if (x > 0) {
+        if (printIt) {
+            uPortLog("%s%s%d SPI resource(s) outstanding.\n", pPrefix, pErrorMarker, x);
+        }
+        resourcesClean = false;
+    }
+    if (resourcesClean && printIt) {
+        uPortLog("%sresources are good (%d outstanding OS resource(s), as expected).\n",
+                 pPrefix, osShouldBeOutstanding);
+    }
+
+    return resourcesClean;
+}
+
+// End of file

port/u_port_heap.c
@@ -15,7 +15,8 @@
  */
 
 /** @file
- * @brief Default implementation of pUPortMalloc() / uPortFree().
+ * @brief Default implementation of pUPortMalloc() / uPortFree() and
+ * uPortHeapAllocCount().
  */
 
 #ifdef U_CFG_OVERRIDE
@@ -34,7 +35,8 @@
 #include "ctype.h"     // isprint()
 
 #include "u_cfg_sw.h"
-#include "u_compiler.h"
+#include "stdint.h"      // int32_t etc.
+#include "u_compiler.h"  // U_WEAK
 
 #include "u_error_common.h"
 #include "u_assert.h"
@@ -121,6 +123,11 @@ typedef struct uPortHeapBlock_t {
  * VARIABLES
  * -------------------------------------------------------------- */
 
+/** Variable to keep track of the total number of heap allocations
+ * outstanding.
+ */
+static int32_t gHeapAllocCount = 0;
+
 #ifdef U_CFG_HEAP_MONITOR
 /** Root of linked list of blocks on the heap.
  */
@@ -184,7 +191,11 @@ static void *_pUPortMalloc(size_t sizeBytes)
 U_WEAK void *pUPortMalloc(size_t sizeBytes)
 #endif
 {
-    return malloc(sizeBytes);
+    void *pMalloc = malloc(sizeBytes);
+    if (pMalloc != NULL) {
+        gHeapAllocCount++;
+    }
+    return pMalloc;
 }
 
 #ifdef U_CFG_HEAP_MONITOR
@@ -300,7 +311,13 @@ U_WEAK void uPortFree(void *pMemory)
     }
 #endif
 
+    gHeapAllocCount--;
     free(pMemory);
+}
+
+U_WEAK int32_t uPortHeapAllocCount()
+{
+    return gHeapAllocCount;
 }
 
 // Print out the contents of the heap.

common/security/test/u_security_tls_test.c
@@ -46,6 +46,8 @@
 #include "u_port_heap.h"
 #include "u_port_debug.h"
 
+#include "u_test_util_resource_check.h"
+
 #include "u_network.h"
 #include "u_network_test_shared_cfg.h"
 
@@ -454,6 +456,8 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsSock")
     (void) heapXxxSockInitLoss;
     (void) heapUsed;
 #endif
+    // Printed for information: asserting happens in the postamble
+    uTestUtilResourceCheck(U_TEST_PREFIX, NULL, true);
 }
 
 /** UDP socket over a DTLS connection.
@@ -712,6 +716,8 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsUdpSock")
     (void) heapXxxSockInitLoss;
     (void) heapUsed;
 #endif
+    // Printed for information: asserting happens in the postamble
+    uTestUtilResourceCheck(U_TEST_PREFIX, NULL, true);
 }
 
 /** Clean-up to be run at the end of this round of tests, just
@@ -720,7 +726,16 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsUdpSock")
  */
 U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsCleanUp")
 {
-    int32_t y;
+    U_TEST_PRINT_LINE("cleaning up any outstanding resources.\n");
+
+    uSockCleanUp();
+    uSockDeinit();
+
+    // Clean-up the sockets thread-safety mutexes and the TLS
+    // security mutex; an application wouldn't normally,
+    // do this, we only do it here to make the sums add up
+    uSockFree();
+    uSecurityTlsCleanUp();
 
     // The network test configuration is shared between
     // the network, sockets, security and location tests
@@ -728,22 +743,9 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsCleanUp")
     // tests of one of the other APIs are coming next.
     uNetworkTestCleanUp();
     uDeviceDeinit();
-
-    y = uPortTaskStackMinFree(NULL);
-    if (y != (int32_t) U_ERROR_COMMON_NOT_SUPPORTED) {
-        U_TEST_PRINT_LINE("main task stack had a minimum of %d byte(s)"
-                          " free at the end of these tests.", y);
-        U_PORT_TEST_ASSERT(y >= U_CFG_TEST_OS_MAIN_TASK_MIN_FREE_STACK_BYTES);
-    }
-
     uPortDeinit();
-
-    y = uPortGetHeapMinFree();
-    if (y >= 0) {
-        U_TEST_PRINT_LINE("heap had a minimum of %d byte(s) free"
-                          " at the end of these tests.", y);
-        U_PORT_TEST_ASSERT(y >= U_CFG_TEST_HEAP_MIN_FREE_BYTES);
-    }
+    // Printed for information: asserting happens in the postamble
+    uTestUtilResourceCheck(U_TEST_PREFIX, NULL, true);
 }
 
 // End of file

commit a5f7a74166dfaa8f4746677dcabb40e4b5919838

IMPORTANT FIXES FOR MEMORY LEAKS. (#1120)
When more organised memory leak checking was introduced in commit 554d008566f9766e2732c66e76872f3bf1c9369f on 29 August 2023 it kind of wasn't: the counting wasn't quite right in that a uPortFree() of a NULL pointer was counted as a true free, when of course it should not be (it is a perfectly valid free(), of course, just shouldn't affect the count of outstanding heap blocks).  This gave us a bye, allowing memory leaks to creep in.

This commit fixes that hole in the leak-checking and also fixes two leaks that were introduced while the hole was present:

- commit 3c734976af305bb96deac7493fb9917ff2db4c3e, which introduced Wi-Fi position to the location API (at about the same time as the memory leak checking change): when Wifi position is requested from the uLocation API a block of 12 bytes (containing `uLocationSharedWifiSettings_t`) would not be free'd afterwards,
- commit d86f0c6223909e30a7ec48f87083f6da104d5132, which introduced the ability to set configuration values from the Zephyr device tree on 26 January 2024: on all platforms (not just Zephyr) up to 20 bytes of heap may leak when a network interface is brought down.

common/device/src/u_device.c
@@ -333,18 +333,22 @@ int32_t uDeviceClose(uDeviceHandle_t devHandle, bool powerOff)
         deviceType = uDeviceGetDeviceType(devHandle);
         switch (deviceType) {
             case U_DEVICE_TYPE_CELL:
+                uNetworkCfgFree(devHandle);
                 errorCode = uDevicePrivateCellRemove(devHandle, powerOff);
                 break;
             case U_DEVICE_TYPE_GNSS:
+                uNetworkCfgFree(devHandle);
                 errorCode = uDevicePrivateGnssRemove(devHandle, powerOff);
                 break;
             case U_DEVICE_TYPE_SHORT_RANGE:
                 if (!powerOff) {
+                    uNetworkCfgFree(devHandle);
                     errorCode = uDevicePrivateShortRangeRemove(devHandle);
                 }
                 break;
             case U_DEVICE_TYPE_SHORT_RANGE_OPEN_CPU:
                 if (!powerOff) {
+                    uNetworkCfgFree(devHandle);
                     errorCode = uDevicePrivateShortRangeOpenCpuRemove(devHandle);
                 }
                 break;
@@ -357,8 +361,6 @@ int32_t uDeviceClose(uDeviceHandle_t devHandle, bool powerOff)
             void *pDeviceType = (void *) deviceType;
             void *pPowerOff = (void *) powerOff;
             errorCode = uDeviceCallback("close", pDeviceType, pPowerOff);
-            // Free any storage allocated for network configuration data
-            uNetworkCfgFree(devHandle);
         }
 
         // ...and done

common/location/src/u_location.c
@@ -182,8 +182,8 @@ static void gnssPosCallback(uDeviceHandle_t devHandle,
                                            pEntry->desiredRateMs, NULL,
                                            pEntry->pCallback);
             }
+            uLocationSharedFifoEntryFree(pEntry);
         }
-        uPortFree(pEntry);
 
         U_PORT_MUTEX_UNLOCK(gULocationMutex);
     }
@@ -233,8 +233,8 @@ static void cellLocCallback(uDeviceHandle_t devHandle,
                            pEntry->type, NULL, NULL,
                            pEntry->pCallback);
             }
+            uLocationSharedFifoEntryFree(pEntry);
         }
-        uPortFree(pEntry);
 
         U_PORT_MUTEX_UNLOCK(gULocationMutex);
     }
@@ -267,7 +267,6 @@ static void wifiPosCallback(uDeviceHandle_t wifiHandle,
                 pApiKey = pWifiSettings->pApiKey;
                 locationAssist.accessPointsFilter = pWifiSettings->accessPointsFilter;
                 locationAssist.rssiDbmFilter = pWifiSettings->rssiDbmFilter;
-                uPortFree((void *) pWifiSettings);
                 // startAsync() might return busy or out of memory temporarily
                 // so give it a few goes and make sure to yield so that OS
                 // things sort themselves out before even trying
@@ -282,7 +281,7 @@ static void wifiPosCallback(uDeviceHandle_t wifiHandle,
                     retryCount++;
                 } while ((x < 0) && (retryCount < 10));
             }
-            uPortFree(pEntry);
+            uLocationSharedFifoEntryFree(pEntry);
         }
 
         U_PORT_MUTEX_UNLOCK(gULocationMutex);
@@ -324,7 +323,7 @@ static int32_t startAsyncGnss(uDeviceHandle_t devHandle,
             errorCode = uGnssPosGetStart(devHandle, gnssPosCallback);
         }
         if (errorCode != 0) {
-            uPortFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_GNSS));
+            uLocationSharedFifoEntryFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_GNSS));
         }
     }
 
@@ -374,7 +373,7 @@ int32_t startAsync(uDeviceHandle_t devHandle, int32_t desiredRateMs,
                                                          pWifiSettings->rssiDbmFilter,
                                                          wifiPosCallback);
                             if (errorCode != 0) {
-                                uPortFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_WIFI));
+                                uLocationSharedFifoEntryFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_WIFI));
                             }
                         } else {
                             uPortFree(pWifiSettings);
@@ -400,7 +399,7 @@ int32_t startAsync(uDeviceHandle_t devHandle, int32_t desiredRateMs,
                     if (errorCode == 0) {
                         errorCode = uCellLocGetStart(devHandle, cellLocCallback);
                         if (errorCode != 0) {
-                            uPortFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_CELL_LOCATE));
+                            uLocationSharedFifoEntryFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_CELL_LOCATE));
                         }
                     }
                 }
@@ -630,18 +629,18 @@ void uLocationGetStop(uDeviceHandle_t devHandle)
 
         int32_t devType = uDeviceGetDeviceType(devHandle);
         if (devType == (int32_t) U_DEVICE_TYPE_SHORT_RANGE) {
-            uPortFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_WIFI));
+            uLocationSharedFifoEntryFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_WIFI));
             uWifiLocGetStop(devHandle);
         } else if (devType == (int32_t) U_DEVICE_TYPE_CELL) {
-            uPortFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_CELL_LOCATE));
+            uLocationSharedFifoEntryFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_CELL_LOCATE));
             uCellLocGetStop(devHandle);
             // Also stop these here in case the GNSS device
             // was being accessed via the cellular device
-            uPortFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_GNSS));
+            uLocationSharedFifoEntryFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_GNSS));
             uGnssPosGetStop(devHandle);
             uGnssPosGetStreamedStop(devHandle);
         } else if (devType == (int32_t) U_DEVICE_TYPE_GNSS) {
-            uPortFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_GNSS));
+            uLocationSharedFifoEntryFree(pULocationSharedRequestPop(U_LOCATION_SHARED_FIFO_GNSS));
             uGnssPosGetStop(devHandle);
             uGnssPosGetStreamedStop(devHandle);
         }

common/location/src/u_location_shared.c
@@ -106,8 +106,7 @@ void uLocationSharedDeinit()
              x < (int32_t) U_LOCATION_TYPE_MAX_NUM;
              x++) {
             while ((pEntry = pULocationSharedRequestPop((uLocationSharedFifo_t) x)) != NULL) {
-                uPortFree((void *) pEntry->pWifiSettings);
-                uPortFree(pEntry);
+                uLocationSharedFifoEntryFree(pEntry);
             }
         }
         U_PORT_MUTEX_UNLOCK(gULocationMutex);
@@ -210,6 +209,15 @@ uLocationSharedFifoEntry_t *pULocationSharedRequestPop(uLocationSharedFifo_t fif
     }
 
     return pSaved;
+}
+
+// Free a FIFO entry.
+void uLocationSharedFifoEntryFree(uLocationSharedFifoEntry_t *pFifoEntry)
+{
+    if (pFifoEntry != NULL) {
+        uPortFree((void *) pFifoEntry->pWifiSettings);
+        uPortFree(pFifoEntry);
+    }
 }
 
 // End of file

common/location/src/u_location_shared.h
@@ -101,6 +101,7 @@ int32_t uLocationSharedInit();
 void uLocationSharedDeinit();
 
 /** Add a new location request to the FIFO.
+ *
  * IMPORTANT: gULocationMutex should be locked before this
  * is called.
  *
@@ -127,6 +128,7 @@ int32_t uLocationSharedRequestPush(uDeviceHandle_t devHandle,
                                                       const uLocation_t *pLocation));
 
 /** Pop the oldest location request of the given FIFO.
+ *
  * IMPORTANT: gULocationMutex should be locked before this
  * is called.
  *
@@ -136,6 +138,16 @@ int32_t uLocationSharedRequestPush(uDeviceHandle_t devHandle,
  *             when done; NULL is returned if the FIFO is empty.
  */
 uLocationSharedFifoEntry_t *pULocationSharedRequestPop(uLocationSharedFifo_t fifo);
+
+/** Free a FIFO entry (e.g. as returned by
+ * pULocationSharedRequestPop()) when done.
+ *
+ * IMPORTANT: gULocationMutex should be locked before this
+ * is called.
+ *
+ * @param[in] pFifoEntry  the FIFO entry to free.
+ */
+void uLocationSharedFifoEntryFree(uLocationSharedFifoEntry_t *pFifoEntry);
 
 #ifdef __cplusplus
 }

common/network/src/u_network_shared.c
@@ -155,6 +155,7 @@ void uNetworkCfgFree(uDeviceHandle_t devHandle)
         for (size_t x = 0; (x < sizeof(pInstance->networkData) /
                             sizeof(pInstance->networkData[0])); x++) {
             uPortFree(pInstance->networkData[x].pCfg);
+            pInstance->networkData[x].pCfg = NULL;
         }
     }
 }

port/platform/common/test_util/u_test_util_resource_check.c
@@ -118,6 +118,7 @@ bool uTestUtilResourceCheck(const char *pPrefix,
     bool resourcesClean = true;
     int32_t x;
     int32_t osShouldBeOutstanding = uPortOsResourcePerpetualCount();
+    int32_t heapShouldBeOutstanding = uPortHeapPerpetualAllocCount();
 
     if (pPrefix == NULL) {
         pPrefix = "";
@@ -157,10 +158,12 @@ bool uTestUtilResourceCheck(const char *pPrefix,
 
     // Check that all heap pUPortMalloc()s have uPortFree()s
     x = uPortHeapAllocCount();
-    if (x > 0) {
+    if (x > heapShouldBeOutstanding) {
         if (printIt) {
-            uPortLog("%s%s%d outstanding call(s) to pUPortMalloc().\n",
-                     pPrefix, pErrorMarker, x);
+            uPortLog("%s%sexpected %d outstanding call(s) to pUPortMalloc()"
+                     " but got %d%s.\n",
+                     pPrefix, pErrorMarker, heapShouldBeOutstanding, x,
+                     (x > heapShouldBeOutstanding) ? "; they might yet be cleaned up" : "");
             uPortHeapDump(pPrefix);
         }
         resourcesClean = false;

port/u_port_heap.c
@@ -316,7 +316,9 @@ U_WEAK void uPortFree(void *pMemory)
     }
 #endif
 
-    gHeapAllocCount--;
+    if (pMemory != NULL) {
+        gHeapAllocCount--;
+    }
     free(pMemory);
 }
 

common/security/test/u_security_tls_test.c
@@ -149,6 +149,10 @@ static const char *const gpEchoServerCaCertPem = "-----BEGIN CERTIFICATE-----\r\
                                                  "iDO9Bnw=\r\n"
                                                  "-----END CERTIFICATE-----";
 
+/** Hook to hold buffer for test data received.
+ */
+static char *gpDataReceived = NULL;
+
 /* ----------------------------------------------------------------
  * STATIC FUNCTIONS
  * -------------------------------------------------------------- */
@@ -209,7 +213,6 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsSock")
     uDeviceHandle_t devHandle = NULL;
     uSockDescriptor_t descriptor = -1;
     uSockAddress_t remoteAddress;
-    char *pDataReceived;
     int32_t startTimeMs;
     size_t sizeBytes;
     size_t offset;
@@ -362,11 +365,12 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsSock")
                           sizeof(gData) - 1, (int32_t) uPortGetTickTimeMs());
 
         // ...and capture them all again afterwards
-        pDataReceived = (char *) pUPortMalloc(sizeof(gData) - 1);
-        U_PORT_TEST_ASSERT(pDataReceived != NULL);
+        uPortFree(gpDataReceived); // In case the previous test failed
+        gpDataReceived = (char *) pUPortMalloc(sizeof(gData) - 1);
+        U_PORT_TEST_ASSERT(gpDataReceived != NULL);
         //lint -e(668) Suppress possible use of NULL pointer
-        // for pDataReceived
-        memset(pDataReceived, 0, sizeof(gData) - 1);
+        // for gpDataReceived
+        memset(gpDataReceived, 0, sizeof(gData) - 1);
         startTimeMs = uPortGetTickTimeMs();
         offset = 0;
         //lint -e{441} Suppress loop variable not found in
@@ -374,7 +378,7 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsSock")
         for (y = 0; (offset < sizeof(gData) - 1) &&
              (uPortGetTickTimeMs() - startTimeMs < 20000); y++) {
             sizeBytes = uSockRead(descriptor,
-                                  pDataReceived + offset,
+                                  gpDataReceived + offset,
                                   (sizeof(gData) - 1) - offset);
             if (sizeBytes > 0) {
                 U_TEST_PRINT_LINE("received %d byte(s) on secure TCP socket.", sizeBytes);
@@ -392,9 +396,10 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsSock")
         }
 
         // Check that we reassembled everything correctly
-        U_PORT_TEST_ASSERT(memcmp(pDataReceived, gData, sizeof(gData) - 1) == 0);
+        U_PORT_TEST_ASSERT(memcmp(gpDataReceived, gData, sizeof(gData) - 1) == 0);
 
-        uPortFree(pDataReceived);
+        uPortFree(gpDataReceived);
+        gpDataReceived = NULL;
 
         // Close the socket
         if (!closeSock(descriptor)) {
@@ -446,7 +451,6 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsUdpSock")
     uDeviceHandle_t devHandle = NULL;
     uSockDescriptor_t descriptor = -1;
     uSockAddress_t remoteAddress;
-    char *pDataReceived;
     int32_t startTimeMs;
     size_t sizeBytes;
     size_t offset;
@@ -599,11 +603,12 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsUdpSock")
                           sizeof(gData) - 1, (int32_t) uPortGetTickTimeMs());
 
         // ...and capture them all again afterwards
-        pDataReceived = (char *) pUPortMalloc(sizeof(gData) - 1);
-        U_PORT_TEST_ASSERT(pDataReceived != NULL);
+        uPortFree(gpDataReceived); // In case the previous test failed
+        gpDataReceived = (char *) pUPortMalloc(sizeof(gData) - 1);
+        U_PORT_TEST_ASSERT(gpDataReceived != NULL);
         //lint -e(668) Suppress possible use of NULL pointer
-        // for pDataReceived
-        memset(pDataReceived, 0, sizeof(gData) - 1);
+        // for gpDataReceived
+        memset(gpDataReceived, 0, sizeof(gData) - 1);
         startTimeMs = uPortGetTickTimeMs();
         offset = 0;
         //lint -e{441} Suppress loop variable not found in
@@ -611,7 +616,7 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsUdpSock")
         for (y = 0; (offset < sizeof(gData) - 1) &&
              (uPortGetTickTimeMs() - startTimeMs < 20000); y++) {
             sizeBytes = uSockRead(descriptor,
-                                  pDataReceived + offset,
+                                  gpDataReceived + offset,
                                   (sizeof(gData) - 1) - offset);
             if (sizeBytes > 0) {
                 U_TEST_PRINT_LINE("received %d byte(s) on secure UDP socket.", sizeBytes);
@@ -629,7 +634,10 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsUdpSock")
         }
 
         // Check that we reassembled everything correctly
-        U_PORT_TEST_ASSERT(memcmp(pDataReceived, gData, sizeof(gData) - 1) == 0);
+        U_PORT_TEST_ASSERT(memcmp(gpDataReceived, gData, sizeof(gData) - 1) == 0);
+
+        uPortFree(gpDataReceived);
+        gpDataReceived = NULL;
 
         // Close the socket
         if (!closeSock(descriptor)) {
@@ -685,6 +693,8 @@ U_PORT_TEST_FUNCTION("[securityTls]", "securityTlsCleanUp")
 
     // Clean-up the TLS security mutex
     uSecurityTlsCleanUp();
+
+    uPortFree(gpDataReceived);
 
     // The network test configuration is shared between
     // the network, sockets, security and location tests